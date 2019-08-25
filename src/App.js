7db6c9197ef5b9480b93793fa29b3b45c79e9feb
Michael Fedell <michaelfedell14@gmail.com>
2019-08-25 13:12:33 -0700

    Add themes to app and replace favicon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
+
+import {createMuiTheme} from "@material-ui/core";
+import {ThemeProvider} from '@material-ui/styles';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 import Header from "./components/layout/Header";
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Home from "./components/Home";
 import Bio from "./components/Bio";
 import Experiences from "./components/Experiences";
 import Projects from "./components/Projects";
 import Photos from "./components/Photos";
 import CV from "./components/CV";
-import {createMuiTheme} from "@material-ui/core";
-import { ThemeProvider } from '@material-ui/styles';
 
 
 const pages = ["Home", "My Story", "Experiences", "Projects", "Photos", "Curriculum Vitae"];
@@ -17,26 +19,45 @@ const links = ["/", "/bio", "/experience", "/projects", "/photos", "/cv"];
 
 const theme = createMuiTheme({
   palette: {
-    primary: { main: "#172252", },
-    secondary: { main: "#ecb937", },
+    primary: {main: "#172252",},
+    secondary: {main: "#ecb937",},
   },
+  typography: {
+    fontFamily: [
+      'Montserrat',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+
 });
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-    <Router>
-      <div className="App">
-        <Header pages={pages} links={links}/>
-        <Route exact path={"/"} component={Home}/>
-        <Route path={"/bio"} component={Bio}/>
-        <Route path={"/experience"} component={Experiences}/>
-        <Route path={"/projects"} component={Projects}/>
-        <Route path={"/photos"} component={Photos}/>
-        <Route path={"/cv"} component={CV}/>
-      </div>
-    </Router>
+      <Router>
+        <Switch>
+          <React.Fragment>
+            <div className="App">
+              <Header pages={pages} links={links}/>
+              <Route exact path={"/"} component={Home}/>
+              <Route path={"/bio"} component={Bio}/>
+              <Route path={"/experience"} component={Experiences}/>
+              <Route path={"/projects"} component={Projects}/>
+              <Route path={"/photos"} component={Photos}/>
+              <Route path={"/cv"} component={CV}/>
+            </div>
+          </React.Fragment>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
