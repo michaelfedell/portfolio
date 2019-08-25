553c876e800b74f4e8b5795aa3abdcc27920b478
Michael Fedell <michaelfedell14@gmail.com>
2019-08-25 16:02:38 -0700

    Add photos (link to vsco)

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import {Container} from "@material-ui/core";
+import makeStyles from "@material-ui/styles/makeStyles";
+import Link from "@material-ui/core/Link";
 
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: theme.spacing(1),
+  },
+  link: {
+    color: "#0056b3",
+  },
+}));
 
 function Photos() {
+  const classes = useStyles();
+
   return (
-    <div>PHOTOS HERE</div>
+    <div className={classes.root}>
+      <Container>
+        <Typography align={"center"}>
+          I have not yet uploaded any photos, but will soon be adding galleries featuring my favorite people, places,
+          and things.<br/>In the meantime, feel free to check out my photo journals on&nbsp;
+          <Link
+            className={classes.link}
+            component={"a"}
+            href={"https://vsco.co/michaelfedell/journal/p/1"}>
+            VSCO
+          </Link>
+        </Typography>
+      </Container>
+    </div>
   );
 }
 
